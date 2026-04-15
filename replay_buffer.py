6c77c2afccbb7c879fbfc81d91be34b8c304541a
Sridhar Kandi <sridhar2199n@gmail.com>
2026-04-15 16:52:39 +0200

    engg: add replay buffer functionality

---
+++ replay_buffer.py
@@ -0,0 +1,31 @@
+import random
+import torch
+from collections import deque
+
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.deque = deque(maxlen=capacity)
+    
+    def push(self, state, action, reward, next_state, done):
+        self.deque.append((state, action , reward, next_state, done))
+    
+    def sample(self, batch_size):
+        batch = random.sample(self.deque, batch_size )
+        states, actions, rewards, next_states,dones = zip(*batch)
+
+        states = torch.FloatTensor(states)
+
+        actions = torch.LongTensor(actions).unsqueeze(1)
+
+        rewards = torch.FloatTensor(rewards).unsqueeze(1)
+
+        next_states = torch.FloatTensor(next_states)
+
+        dones = torch.FloatTensor(dones).unsqueeze(1)
+        
+        return states, actions, rewards, next_states, dones
+    
+    
+    def __len__(self):
+        return len(self.deque)
